833f99d7782f4783907129924f6435500801ac74
mdilawar-dev <77153555+mdilawar-dev@users.noreply.github.com>
2021-02-17 16:24:49 +0530

    update

--- public/script.js
+++ public/script.js
@@ -1,12 +1,20 @@
-
-    
-// api url 
-const api_url =  
-      "/users"; 
-
-      const api_url2="/history";
   
+// api url 
+const api_url = "/users"; 
+const api_url2="/history";
+  
+// Function to hide the loader 
+function hideloader() { 
+  document.getElementById('loading').style.display = 'none'; 
+}
+
+
+
 // Defining async function 
+
+
+
+
 
 
  async function getapi(url ,url2) { 
@@ -23,129 +31,34 @@ const api_url =
     if (response) { 
         hideloader(); 
     } 
-
-    let tab =  
-    `
-    <thead class="thead-light">
-    <tr> 
-       
-      <th>acc_id</th>
-        <th>Name</th> 
-           <th>Email</th>
-             <th>Current Balance</th> 
-             <th>view user</th> 
-     </tr>
-     </thead>  
-     
-     `; 
-
-// Loop to access all rows  
-for (let r of data.data) { 
-    tab += 
-   
-    
-    `<tr>  <tbody id="myTable">
-<td ">${r.id}</td>
-<td>${r.name} </td> 
-<td>${r.email}</td>  
-<td>${r.balance}</td>  
-<td> <a href="#${r.id}" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">View</a></td>      
-</tr>
-</tbody>
-
-`; 
-} 
-
-
-let t;
-for (let r of data.data) { 
-    t += 
-`
-<option>${r.email}</option>
-`;
- 
-
-}
-
-document.getElementById("users").innerHTML = tab; 
-document.getElementById("send_to").innerHTML =t; 
-/**********************************************************/
-let tab2 =  
-`
-<thead class="thead-dark">
-    <tr> 
-       
-      <th>change_no</th>
-        <th>send_form</th> 
-           <th>-/+</th>
-             <th>send_to</th> 
-             <th></th>
-             <th>Amount</th> 
-             <th>transfered_at</th>
-             
-             
-     </tr>
-     </thead> 
-`;
-
-for (let r of data2.data) { 
-    tab2 += 
-` 
-<tr>   
-<td scope="row">${r.change_no}</td>
-<td>${r.Debited}</td>  
-<td>${r.flag}</td> 
-<td>${r.Credited}<td>
-<td>${r.amount}</td>
-<td>${r.transfered_at}</td>
-</tr>
-`;
- 
-
-}
-
-document.getElementById("tra").innerHTML = tab2; 
-
-const allusers=document.getElementById('allusers');
-const all_trc=document.getElementById('all_trc');
-const trn_money=document.getElementById('trn_money');
-const welcome=document.getElementById("welcome");
-
-allusers.style.display='none';
-all_trc.style.display='none';
-trn_money.style.display='none';
-
-
-document.getElementById("show").addEventListener("click", function() {
-    all_trc.style.display='none';
-    welcome.style.display='none';
-   trn_money.style.display = 'block';
-   allusers.style.display = 'block';
-
-  });
-
-  document.getElementById("trac_history").addEventListener("click", function() {
-  allusers.style.display='none';
-    trn_money.style.display = 'none';
-    all_trc.style.display = 'block';
-    welcome.style.display = 'none';
-  });
+  
+    interface(data,data2);
    
 
-  const idd1=document.getElementById('send_form');
-  const idd2 =document.getElementById('send_to');
-  const amount_to_send =document.getElementById('send_amount');
-
-  document.getElementById('send1').addEventListener('click',()=>{
-   console.log("helo")
-       update_data(idd1.value,idd2.value,amount_to_send.value)
- 
- });
-
 } 
-// Calling that async function 
+
+   // Calling that async function 
 getapi(api_url ,api_url2); 
+
+
+async function getUser(){
+     
+  let userID = window.location.hash.replace('#', '');
+  if(userID === ''){
+         console.log("no user ID yet");
+  }else {
+  const userResponse = await fetch(`/users/userID/${userID}`);
+  var data = await userResponse.json(); 
+
+ setuser(data);
+   
+  }
  
+
+
+}
+
+
 
 
 async function update_data(id1,id2,amount){
@@ -162,7 +75,10 @@ try{
     }
     ).then(response => response.json()).then(json => alert(json.message))
 
-     fetch(`/`);
+    getapi(api_url ,api_url2); 
+    document.getElementById("send_form").innerHTML = ''; 
+    document.getElementById('userprofile').innerHTML='';
+  
 }catch(error){
 
     alert("Something went wrong Check all details");
@@ -172,42 +88,11 @@ try{
 }
      
 
-// Function to hide the loader 
-function hideloader() { 
-    document.getElementById('loading').style.display = 'none'; 
-} 
+
 // Function to define innerHTML for HTML table 
 
     
-    let t1;
-    let userprofile ;
-    async function getUser(){
-      let userID = window.location.hash.replace('#', '');
-      if(userID === ''){
-             console.log("no user ID yet");
-      }else {
-      const userResponse = await fetch(`/users/userID/${userID}`);
-      var data = await userResponse.json(); 
-      window.location.hash='';
-      //console.log(data.data.email);
-
-      
-       userprofile = 
-      `
-      <span class="font-weight-bold">acc_id:${data.data.id}<br>User:${data.data.name} <br>Email:${data.data.email}<br>current_Balance:${data.data.balance}</span>
-     `;
-     
-          t1 = 
-      `
-      <option>${data.data.email}</option>
-     `;
-       
-      }
-      document.getElementById("send_form").innerHTML = t1; 
-      document.getElementById('userprofile').innerHTML=userprofile;
-
-
-    }
+   
    
    
 //window.addEventListener('hashchange', controlRecipe);
@@ -228,3 +113,152 @@ function hideloader() {
       });
 */
 
+
+function setuser(data){
+  let t1;
+  let userprofile ;
+  window.location.hash='';
+  //console.log(data.data.email);
+
+  
+   userprofile = 
+  `
+  <span class="font-weight-bold">acc_id:${data.data.id}<br>User:${data.data.name} <br>Email:${data.data.email}<br>current_Balance:${data.data.balance}</span>
+ `;
+ 
+      t1 = 
+  `
+  <option>${data.data.email}</option>
+ `;
+
+
+ document.getElementById("send_form").innerHTML = t1; 
+ document.getElementById('userprofile').innerHTML=userprofile;
+
+  
+}
+
+
+function interface(data,data2){
+
+  let tab =  
+  `
+  <thead class="thead-light">
+  <tr> 
+     
+    <th>acc_id</th>
+      <th>Name</th> 
+         <th>Email</th>
+           <th>Current Balance</th> 
+           <th>view user</th> 
+   </tr>
+   </thead>  
+   
+   `; 
+
+// Loop to access all rows  
+for (let r of data.data) { 
+  tab += 
+ 
+  
+  `<tr>  <tbody id="myTable">
+<td ">${r.id}</td>
+<td>${r.name} </td> 
+<td>${r.email}</td>  
+<td>${r.balance}</td>  
+<td> <a href="#${r.id}" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">View</a></td>      
+</tr>
+</tbody>
+
+`; 
+} 
+
+
+let t;
+for (let r of data.data) { 
+  t += 
+`
+<option>${r.email}</option>
+`;
+
+
+}
+
+document.getElementById("users").innerHTML = tab; 
+document.getElementById("send_to").innerHTML =t; 
+/**********************************************************/
+let tab2 =  
+`
+<thead class="thead-dark">
+  <tr> 
+     
+    <th>change_no</th>
+      <th>send_form</th> 
+         <th>-/+</th>
+           <th>send_to</th> 
+           <th></th>
+           <th>Amount</th> 
+           <th>transfered_at</th>
+           
+           
+   </tr>
+   </thead> 
+`;
+
+for (let r of data2.data) { 
+  tab2 += 
+` 
+<tr>   
+<td scope="row">${r.change_no}</td>
+<td>${r.Debited}</td>  
+<td>${r.flag}</td> 
+<td>${r.Credited}<td>
+<td>${r.amount}</td>
+<td>${r.transfered_at}</td>
+</tr>
+`;
+
+
+} 
+
+document.getElementById("tra").innerHTML = tab2; 
+
+const allusers=document.getElementById('allusers');
+const all_trc=document.getElementById('all_trc');
+const trn_money=document.getElementById('trn_money');
+const welcome=document.getElementById("welcome");
+
+allusers.style.display='none';
+all_trc.style.display='none';
+trn_money.style.display='none';
+
+
+document.getElementById("show").addEventListener("click", function() {
+  
+  all_trc.style.display='none';
+  welcome.style.display='none';
+ trn_money.style.display = 'block';
+ allusers.style.display = 'block';
+
+});
+
+document.getElementById("trac_history").addEventListener("click", function() {
+  welcome.style.display = 'none';
+allusers.style.display='none';
+  trn_money.style.display = 'none';
+  all_trc.style.display = 'block';
+  
+});
+ 
+
+const idd1=document.getElementById('send_form');
+const idd2 =document.getElementById('send_to');
+const amount_to_send =document.getElementById('send_amount');
+
+document.getElementById('send1').addEventListener('click',()=>{
+ console.log("helo")
+     update_data(idd1.value,idd2.value,amount_to_send.value)
+
+});
+
+}
